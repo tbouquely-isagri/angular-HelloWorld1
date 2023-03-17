568ff3953fcedd5dc3f710184265d5dfacd38db5
thibaud60 <128160819+thibaud60@users.noreply.github.com>
2023-03-17 19:31:37 +0100

    2 files modified

--- src/main.ts
+++ src/main.ts
@@ -11,18 +11,32 @@ import { bootstrapApplication } from '@angular/platform-browser';
     <h1>{{name}}!</h1>
     <h2>Prototype de repository de regle de gestion de validation d'entité sur base d'une entité de référence</h2>
     
+    <h1>Résumé</h1>
+
+    il est possible avec la méthode call ou apply de la classe statique Object de TS/JS d'appeler une méthode d'un object en lui passant un 
+    this dynamique !
+    le this en question peut etre un simple flux json / object à partir du moment ou les champs sont bien identique en type et nbre
+    par contre, si la méthode appelée appel elle même d'autre méthode de la classe courante
+    ça ne fonctionne pas car l'objet qui se subtitue à this n'as pas la méthode en question, c'est logique !
+
     <h1>Experimentation</h1>
     <button type="button" (click)="onClick001()">Clic me ! 001</button>
     <h2>Résultats aprés le clic ...</h2>
     <ul>
         <!-- cf https://stackblitz.com/edit/angular-listbox?file=src%2Fapp%2Fapp.component.html -->
-        <li *ngFor="let item of resultats">{{item.text}}</li>
+        <li 
+          *ngFor="let item of resultats"
+          [class.item-selected]="item.selected">{{item.text}}"
+        </li>
     </ul>
   `,
 })
 export class App {
   name = 'Angular HelloWorld 001';
   resultats = [];
+  private AddCommentaires(commentaire: string) {
+    this.resultats.push({ text: commentaire, selected: true });
+  }
   private AddResultat(
     title: string,
     actualValue: string,
@@ -32,12 +46,11 @@ export class App {
     f('----------------------------------');
     this.resultats.push(title);
     f('  ' + title);
-    f('  actual = ' + actualValue);
-    f('  expected = ' + expectedValue);
+    f('  actual = ' + actualValue + ', expected = ' + expectedValue);
     if (actualValue == expectedValue) {
       f('  identique');
     } else {
-      f('   DIFFERENT DIFFERENT DIFFERENT DIFFERENT');
+      this.resultats.push({ text: 'DIFFERENT', selected: true });
     }
   }
 
@@ -95,6 +108,9 @@ export class App {
       emptyPersonne.PoidsValidate(),
       '|0'
     );
+    /* https://www.w3schools.com/js/js_function_apply.asp#:~:text=The%20Difference%20Between%20call(),instead%20of%20an%20argument%20list.
+    The difference is: The call() method takes arguments separately. The apply() method takes arguments as an array. The apply() method is very handy if you want to use an array instead of an argument list.
+    */
     this.AddResultat(
       'E1 : On utilise sa méthode de validation via APPLY, OK = ',
       emptyPersonne.PoidsValidate.apply(bob3),
@@ -111,15 +127,21 @@ export class App {
       '|0'
     );
     this.AddResultat(
-      "E1 : On tente maintenant d'utiliser un objet TS de structure identique mais ce n'est pas une Personne, APPLY = ",
-      emptyPersonne.PoidsValidate.apply(bob2AsSimpleObjectFromJSon),
-      'bob2|72'
-    );
-    this.AddResultat(
       "E1 : On tente maintenant d'utiliser un objet TS de structure identique mais ce n'est pas une Personne, CALL = ",
       emptyPersonne.PoidsValidate.call(bob2AsSimpleObjectFromJSon),
       'bob2|72'
     );
+    try {
+      this.AddResultat(
+        "E1 : est que le 'this' est passé de proche en proche dans les méthodes de EmptyPersonne ? NON, CA NE PEUX PAS FONCTIONNER, CALL = ",
+        emptyPersonne.PoidsValidateWithSubMethod.call(
+          bob2AsSimpleObjectFromJSon
+        ),
+        'bob2|72'
+      );
+    } catch (e: any) {
+      this.AddCommentaires('ERREUR ' + e);
+    }
     this.AddResultat(
       'E1 : notre personne vide est toujours vide ! = ',
       emptyPersonne.PoidsValidate(),
@@ -139,6 +161,9 @@ class Personne {
   public PoidsValidate() {
     // this ici pointe bien sur l'instance de la class Personne
     return Personne.PoidsValidateOn(this);
+  }
+  public PoidsValidateWithSubMethod() {
+    this.PoidsValidate();
   }
 
   public static PoidsValidateOn(item: Personne) {
